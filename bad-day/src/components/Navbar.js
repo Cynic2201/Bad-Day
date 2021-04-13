50cdae47f9ac6c63de5bc08f8d145d93b0b341aa
Grant Smoot <gsmoot@uncc.edu>
2021-04-13 18:07:34 -0400

    Added skeleton of navbar, fixed index.html

---
+++ bad-day/src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className="navBar">
+            <a href="index.html">Home</a>
+            <a href="report.html">Statistics</a>
+            <a id="register" href="register.html">Login/Signup</a>
+        </div>
+    )
+}
+
+export default Navbar
