5e9faf4c7628ad99965e24c009d51f8f89060632
pokemon2201 <62266523+pokemon2201@users.noreply.github.com>
2021-04-16 15:06:00 -0400

    HTML implemented

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
 const Home = () => {
     return <div>
-        <h1>Home page</h1>
-    </div>
+    <html>
+        <head>
+            <title>Bad Days: Home Page</title>
+            <link rel="stylesheet" href="./index.css" type="text/css" />
+        </head>
+
+        <body>
+            <div class="container">
+                <b>
+                    <div class="title">BAD DAY</div>
+                    <img class="center-cropped" src="images\2923.jpg" />
+                    <button class="btn1" onclick="location.href='register.html';"><b>LOGIN/SIGN-UP</b></button>          
+                    <h1 class="text">OR</h1>
+                    <button class="btn2" onclick="location.href='report.html';"><b>START NOW</b></button>
+                </b>
+            </div>     
+        </body>
+    </html>
+</div>
 }
 
 export default Home;
--- src/components/LoginSignup.js
+++ src/components/LoginSignup.js
@@ -1,7 +1,56 @@
 const LoginSignup = () => {
     return <div>
-        <h1>Login/Signup page</h1>
-    </div>
+    <html>
+        <head>
+            <title>Login/Sign-up</title>
+            <link rel="stylesheet" href="./register.css" type="text/css"/>
+        </head>
+
+        <body>
+            <form class = "alignForm">
+                <div class = "center">
+                <div class="leftDiv">
+                    <b><label for="login">Login</label></b><br></br>
+                    <label for="email">Email:</label><br></br>
+                    <label for="lname">Password:</label><br></br>
+                </div>
+
+                <div class="inputRightDiv">
+                    <br></br>
+                    <input type="text" id="email" name="email"></input><br></br>
+                    <input type="text" id="password" name="password"></input><br></br>
+                </div>
+                </div>
+            </form>
+
+            <form class = "alignForm" id="signupForm">
+                <div class="leftDiv">
+                    <b><label for="signup">Sign-up</label></b><br></br>
+                    <label for="fname">First Name:</label><br></br>
+                    <label for="lname">Last Name</label><br></br>
+                    <label for="email">Email:</label><br></br>
+                    <label for="email">Repeat Email:</label><br></br>
+                    <label for="password">Password:</label><br></br>
+                    <label for="password">Repeat Password:</label><br></br>
+                </div>
+                
+                <div class="rightDiv">
+                    <br></br>
+                    <input type="text" id="fname" name="fname"></input><br></br>
+                    <input type="text" id="lname" name="lname"></input><br></br>
+                    <input type="text" id="email" name="email"></input><br></br>
+                    <input type="text" id="email" name="email"></input><br></br>
+                    <input type="text" id="password" name="password"></input><br></br>
+                    <input type="text" id="lname" name="lname"></input><br></br>
+                    <input type="submit" value="SIGN-UP"></input><br></br>
+                </div>
+                
+            </form>
+        </body>
+        <div class = "bottomBar">
+        </div>
+    </html>
+</div>
 }
 
 export default LoginSignup;
