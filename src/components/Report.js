4afc96336d55d0aa93fe802603b2419bf210c94e
Grant Smoot <gsmoot@uncc.edu>
2021-04-16 03:39:37 -0400

    Update Report.js

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,70 +1,73 @@
+import React, { Component } from "react";
 import Checkbox from "./Checkbox";
 
 const DISASTERS = ["Natural", "Disease", "Crime", "Accident"];
 
 
 
-const Report = () => {
-    const handleCheckboxSubmit = formSubmitEvent => {
+class Report extends Component {
+    handleCheckboxSubmit = formSubmitEvent => {
         formSubmitEvent.preventDefault();
         //function to organize what data to present
     };
-    
-    const handleProfileSave = formSubmitEvent => {
+
+    handleProfileSave = formSubmitEvent => {
         formSubmitEvent.preventDefault();
         //function to organize what data to present
     };
-    
-    const createCheckbox = disaster => (
+
+    createCheckbox = disaster => (
         <>
-        <Checkbox
-            label={disaster}
-            key={disaster}
-        />
-        <br />
+            <Checkbox
+                label={disaster}
+                key={disaster}
+            />
+            <br />
         </>
     );
-    
-    const createCheckboxes = () => DISASTERS.map(this.createCheckbox);
-    return (
-        <>
-            <form className="alignForm" id="profileForm">
-                <div className="leftDiv">
-                    <label>Select Profile</label><br />
-                    <label>Profile Name: </label><br />
-                    <label>Age: </label><br />
-                    <label>Location: </label><br />
-                </div>
-                <div className="rightDiv">
-                    <select id="selectProfile">
 
-                    </select><br />
-                    <input type="text" /><br />
-                    <input type="text" /><br />
-                    <input type="text" placeholder="location" /><br />
-                    <input type="button" placeholder="Find My Location" /><br />
-                    <input type="submit" id="centerButton" value="Save Profile" />
-                </div>
-            </form>
-            <form className="alignForm" id="statsForm">
-                <div class="leftDiv">
-                    <label>Natural Disasters :</label><br />
-                    <label>Diseases and Illnesses :</label><br />
-                    <label>Crimes :</label><br />
-                    <label>Vehicle Accidents :</label><br />
-                    <label>Unnatural Disasters :</label><br />
-                </div>
-                <div className="rightDiv">
-                    {this.createCheckboxes()}
-                    <button type="submit">Get Stats</button>
-                </div>
-            </form>
-            <div className="reportBody">
+    createCheckboxes = () => DISASTERS.map(this.createCheckbox);
+    render() {
+        return (
+            <>
+                <form className="alignForm" id="profileForm">
+                    <div className="leftDiv">
+                        <label>Select Profile</label><br />
+                        <label>Profile Name: </label><br />
+                        <label>Age: </label><br />
+                        <label>Location: </label><br />
+                    </div>
+                    <div className="rightDiv">
+                        <select id="selectProfile">
 
-            </div>
-            <div className="bottomBar" />
-        </>
-    )
+                        </select><br />
+                        <input type="text" /><br />
+                        <input type="text" /><br />
+                        <input type="text" placeholder="location" /><br />
+                        <input type="button" placeholder="Find My Location" /><br />
+                        <input type="submit" id="centerButton" value="Save Profile" />
+                    </div>
+                </form>
+                <form className="alignForm" id="statsForm">
+                    <div class="leftDiv">
+                        <label>Natural Disasters :</label><br />
+                        <label>Diseases and Illnesses :</label><br />
+                        <label>Crimes :</label><br />
+                        <label>Vehicle Accidents :</label><br />
+                        <label>Unnatural Disasters :</label><br />
+                    </div>
+                    <div className="rightDiv">
+                        {this.createCheckboxes()}
+                        <button type="submit">Get Stats</button>
+                    </div>
+                </form>
+                <div className="reportBody">
+
+                </div>
+                <div className="bottomBar" />
+            </>
+        )
+    }
 }
 
-export default Report
+export default Report;
