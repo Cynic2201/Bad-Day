2dd2c97d830f64eca53029dbd4851459c4ef0710
pokemon2201 <62266523+pokemon2201@users.noreply.github.com>
2021-04-16 16:24:11 -0400

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 function Navbar() {
-    return <nav className="navBar">
-        <h4 className="navBarItem"><Link to="/">Home</Link></h4>
-        <h4 className="navBarItem"><Link to="/Report">Disaster Report</Link></h4>
-        <h4 id="register"><Link to="/LoginSignup">Login/Signup</Link></h4>
+    return<nav className="navBar">
+        <link rel="stylesheet" href="./index.css" type="text/css" />
+        <a class="navBarItem"><Link to="/">Home</Link></a>
+        <a class="navBarItem"><Link to="/Report">Disaster Report</Link></a>
+        <a id="register"><Link to="/LoginSignup">Login/Signup</Link></a>
     </nav>
 }
 
