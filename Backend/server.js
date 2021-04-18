26de4784a27a01777e40037803abacd71a8028ac
Isakterzic <isak.terzic@gmail.com>
2021-04-18 17:48:11 -0400

    Connected Python to backend
    server was down so I could not test it running perfectly but localhost 5000 worked

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+//Import PythonShell module.
+const {PythonShell} =require('python-shell');
 
 app.use(cors());
 app.use(express.json());
@@ -17,9 +19,25 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
-const usersRouter = require('./Routes/users');
+app.get("/", (req, res, next)=>{
+    //Here are the option object in which arguments can be passed for the python_test.js.
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time
+        args: ['shubhamk314'] //An argument which can be accessed in the script using sys.argv[1]
+    };
+      
+  
+    PythonShell.run('FindStat.py', options, function (err, result){
+          if (err) throw err;
+          // result is an array consisting of messages collected 
+          //during execution of script.
+          //console.log('result: ', result.toString());
+          res.send(result.toString())
+    });
+});
 
-app.use('/users',usersRouter);
+
 
 app.listen(port, () => {
     console.log("Server is running on Port: " + port);
