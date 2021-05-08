88ceb31ab59ea526c0faad9de17ebecfd9e65128
Grant Smoot <gsmoot@uncc.edu>
2021-05-07 22:50:26 -0400

    did some stuff I guess lol

--- src/components/Report.js
+++ src/components/Report.js
@@ -76,7 +76,10 @@ class Report extends Component {
           )
       }
 
-      /*createInfo(disasterInfo) {
+      createInfo() {
+        var disasterInfo = "0.0044695030193568205, 0.1541750764061035, 0.03002108992956433,"+
+        "0.04835971896595137, 0.015648322292572604, 0.03316189018269106, 0.011353448779634597,"+
+        "0.1711596940521418, 0.04614774521797976, nan, ";
         //This method will slice part of the string and shove it into the info variable
         var info = disasterInfo.slice(0, disasterInfo.indexOf(','));
         //Edit the disasterInfo string so that the sliced bit is taken off
@@ -117,23 +120,18 @@ class Report extends Component {
         disasterInfo = disasterInfo.slice(disasterInfo.indexOf(',')+1);
         info8.replaceAll(',','');
 
-        var info9 = disasterInfo.slice(0, 1);
-        disasterInfo = disasterInfo.slice(disasterInfo.indexOf(',')+1);
-        info9.replaceAll(',','');
-
         display = <div>
-        <p>Disaster: Sepsis. Chance of death: {info}</p><br />
-        <p>Disaster: Cancer. Chance of death: {info1}</p><br />
-        <p>Disaster: Diabetes. Chance of death: {info2}</p><br />
-        <p>Disaster: Alzheimers. Chance of death: {info3}</p><br />
-        <p>Disaster: Flu. Chance of death: {info4}</p><br />
-        <p>Disaster: CLRD. Chance of death: {info5}</p><br />
-        <p>Disaster: Kidney Disease. Chance of death: {info6}</p><br />
-        <p>Disaster: Heart Disease. Chance of death: {info7}</p><br />
-        <p>Disaster: Stroke. Chance of death: {info8}</p><br />
-        <p>Disaster: Covid. Chance of death: {info9}</p><br />
+        <p>Disaster: Sepsis || Percent chance of death: {info}</p><br />
+        <p>Disaster: Cancer || Percent chance of death: {info1}</p><br />
+        <p>Disaster: Diabetes || Percent chance of death: {info2}</p><br />
+        <p>Disaster: Alzheimers || Percent chance of death: {info3}</p><br />
+        <p>Disaster: Flu || Percent chance of death: {info4}</p><br />
+        <p>Disaster: CLRD || Percent chance of death: {info5}</p><br />
+        <p>Disaster: Kidney Disease || Percent chance of death: {info6}</p><br />
+        <p>Disaster: Heart Disease || Percent chance of death: {info7}</p><br />
+        <p>Disaster: Stroke || Percent chance of death: {info8}</p><br />
          </div>;
-}*/
+}
       
     handleCheckboxSubmit = formSubmitEvent => {
         formSubmitEvent.preventDefault();
@@ -191,7 +189,7 @@ class Report extends Component {
                             <option>Stroke</option>
                             <option>Covid</option>
                         </select><br />
-                        <input type="submit" id="centerButton" value="Submit" /*onClick = {this.createInfo(serverInfo)}*/ />
+                        <input type="submit" id="centerButton" value="Submit" onClick = {this.createInfo()} />
                     </div>
                 </form>
                 <form className="alignForm">
